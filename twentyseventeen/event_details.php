c0a63d68a4cd3e396a853f7cd0c6d26388a866f6
SneakyMeligy <sherifkayed94@gmail.com>
2016-12-25 12:12:52 +0200

    Add files via upload

---
+++ twentyseventeen/event_details.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Template Name: EventsDetails_Template
+*/
+ 
+get_header(); ?>
+
+<?php
+$servername = "localhost";
+$username = "webuser";
+$password = "12345";
+$dbname = "webdb";
+//$num_of_events=0;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
+$eventId=$_GET['eventNumber'];
+
+$sql = "SELECT * FROM events WHERE eventNumber={$eventId} ";
+$result = $conn->query($sql);
+
+$remove_sql = "SELECT * FROM events WHERE eventNumber={$eventId} ";
+//public function remove ($value='')
+//{
+  //  $result = $conn->query($remove_sql);
+//}
+
+$reserve_sql = "Insert into tickets ()";
+
+//echo $result->fetch_assoc();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo "Name: " . $row["eventName"]. "Description: " . $row["description"]. "Date: " . $row["date"].  "Hall Number " . $row["hallNumber"]."<br>";
+        echo "<ul>";
+        echo "<table width='200'>";
+        echo "<tr>";
+        echo"<td>" ;
+        echo "Name:  " . $row["eventName"]. "</td>";
+        echo "<td>" ;
+        echo "Description: "  . $row["description"]. "</td>";
+        echo "<td>" ;
+        echo "Date: "  . $row["date"]. "</td>";
+        echo "<td>";
+        echo "Hall Number: "  . $row["hallNumber"]. "</td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td>";
+        echo "Reserve<strong> <a href=http://127.0.0.1/wordpress/tickets/?eventNumber={$row['eventNumber']}>{$row['eventName']}</a>\n</strong></td>";
+        echo "</tr>";
+        echo "</table>";
+        echo "</ul>";
+        //echo "<button (click)="remove($row['id'])">X</button>";
+        //echo "<button (click)="reserve($row['id'])">R</button>";
+    }
+} else {
+    echo "0 events found";
+}
+
+$conn->close();
+?>
+
+<html>
+
+    <form name="testForm" id="testForm"  method="POST"  >
+        <input type="submit" name="remove" value="Submit" autofocus  onclick="return true;"/>
+    </form>
+ <?php
+    if(isset($_POST['remove'])){
+        $result = $conn->query($remove_sql);
+     }
+  ?>
+</html>
---
+++ twentyseventeen/events2.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Events_Template
+*/
+ 
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+$servername = "localhost";
+$username = "webuser";
+$password = "12345";
+$dbname = "webdb";
+//$num_of_events=0;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM events";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<ul>";
+        echo "<table width='200'>";
+        echo "<tr>";
+        echo"<td>" ;
+        echo "Name:<strong> <a href=http://127.0.0.1/wordpress/event-details/?eventNumber={$row['eventNumber']}>{$row['eventName']}</a>\n</strong></td>";
+        echo "<td>" ;
+        echo "Description: "  . $row["description"]. "</td>";
+        echo "<td>" ;
+        echo "Date: "  . $row["date"]. "</td>";
+        echo "<td>";
+        echo "Hall Number: "  . $row["hallNumber"]. "</td>";
+        echo "</tr>";
+        echo "</table>";
+        //echo "<a href=http://127.0.0.1/wordpress/event-details/?eventNumber={$row['eventNumber']}>{$row['eventName']}</a><br>\n";
+        //echo "<button (click)="remove($row['id'])">X</button>";
+        echo "</ul>";
+    }
+} else {
+    echo "0 events found";
+}
+$conn->close();
+?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
